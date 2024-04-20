962e805a67aa1774319443ad071c4ae20b00da9d
barry <lavidesbarry@gmail.com>
2024-04-20 14:37:13 +0800

    multiple messages while persisting the server

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,12 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
     let (mut socket, _addr) = listener.accept().await.unwrap();
-    let mut buffer = [0u8; 1024];
 
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
+    loop {
+        let mut buffer = [0u8; 1024];
 
-    socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        let bytes_read = socket.read(&mut buffer).await.unwrap();
+
+        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    }
 }
